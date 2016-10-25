ac288d99309f57453f69248fa3227af1e15a5326
刘发叶 <ewenlaz@gmail.com>
2016-10-25 16:39:05 +0800

    更新64转化支持

--- beehive.c
+++ beehive.c
@@ -27,6 +27,30 @@
 #include "ext/standard/info.h"
 #include "php_beehive.h"
 
+
+#ifndef _NTOHLL_H
+#define _NTOHLL_H
+
+#ifdef __WINDOWS__
+#define ntohll(x) (((int64_t)(ntohl((int)((x << 32) >> 32))) << 32) | \
+                   (unsigned int)ntohl(((int)(x >> 32)))) //By Runner
+#define htonll(x) ntohll(x)
+#else
+#if __BYTE_ORDER == __BIG_ENDIAN
+# define ntohll(x)   (x)
+# define htonll(x)   (x)
+#else
+# if __BYTE_ORDER == __LITTLE_ENDIAN/* LB: removed that, didn't compile: && defined __bswap_64 */
+#  define ntohll(x) __bswap_64 (x)
+#  define htonll(x) __bswap_64 (x)
+# else
+#  error No function/macros available to switch between uint64_t endianness.
+# endif
+#endif
+#endif
+
+#endif
+
 #define BEEHIVE_FIXED_HEADER_LEN 28
 #if PHP_MAJOR_VERSION < 7
 static inline int beehive_zend_hash_find(HashTable *ht, char *k, int len, void **v)
